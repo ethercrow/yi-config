6f47833456245aa7e34682f566089f696ecb62ac
Dmitry Ivanov <ethercrow@gmail.com>
2015-05-23 17:53:14 +0600

    Add a snippet for cabal test suite.

--- modules/MySnippets.hs
+++ modules/MySnippets.hs
@@ -6,9 +6,7 @@ module MySnippets
 
 import Control.Applicative
 
-import Yi
 import qualified Yi.Rope as R
-
 import Snippet
 
 mySnippets :: [Snippet]
@@ -34,6 +32,15 @@ mySnippets =
         nl
         line "if __name__ == '__main__':"
         line "    main()"
+    , Snippet "testsuite" $ do
+        lit "Test-Suite " >> place "TestMain" >> nl
+        line "  type: exitcode-stdio-1.0"
+        lit "  main-is " >> place "TestMain.hs" >> nl
+        line "  hs-source-dirs: src, test"
+        line "  ghc-options: -Wall -ferror-spans"
+        line "  default-language:    Haskell2010"
+        line "  build-depends:"
+        line "    base >= 4.6"
     , Snippet "cl" $ do
         className <-
             lit "@interface " *> place "ShinyClass" <* lit ": NSObject" <* nl
@@ -42,4 +49,3 @@ mySnippets =
         lit "@implementation " >> mirror className >> nl
         line "@end"
     ]
-
