9aba423a1b1eb74f07f4e6ad69cc13d264d256bd
Dmitry Ivanov <ethercrow@gmail.com>
2015-09-07 23:26:30 +0600

    Update to language-lua2

--- modules/LuaMode.hs
+++ modules/LuaMode.hs
@@ -11,7 +11,6 @@ import Data.Data.Lens
 import Language.Lua.Lexer
 import Language.Lua.Parser
 import Language.Lua.Syntax
-import Language.Lua.Token
 
 import Yi.Buffer.Misc
 import Yi.Modes
@@ -28,12 +27,13 @@ luaMode = fundamentalMode
     , modeGetStrokes = luaGetStrokes
     }
 
+luaHighlighter :: Highlighter LuaStorage LuaStorage
 luaHighlighter = SynHL
     { hlStartState = Nothing
     , hlRun = \scanner _point _oldTokens ->
         let contentString = fmap snd (scanRun scanner (scanInit scanner))
             tokens = streamToList (runLexer luaLexer "" contentString)
-        in case fullParses (parser luaChunk tokens) of
+        in case fullParses (parser luaChunk) tokens of
             ([x], _) -> Just x
             _ -> Nothing
     , hlGetTree = \tokens _windowRef -> tokens
@@ -44,8 +44,11 @@ luaGetStrokes :: LuaStorage -> Point -> Point -> Point -> [Stroke]
 luaGetStrokes mchunk _point _begin _end =
     mapMaybe expToStroke (toListOf tinplate mchunk)
 
-expToStroke (String info _) = Just (mkStroke info stringStyle)
+expToStroke :: Expression NodeInfo -> Maybe Stroke
+expToStroke (String info _) = mkStroke info stringStyle
 expToStroke _ = Nothing
 
+mkStroke :: NodeInfo -> StyleName -> Maybe Stroke
 mkStroke (NodeInfo (Loc (Pos _ _ _ begin) (Pos _ _ _ end)) _) sty =
-    Span (Point begin) sty (Point (end + 1))
+    Just (Span (Point begin) sty (Point (end + 1)))
+mkStroke (NodeInfo NoLoc _) sty = Nothing
